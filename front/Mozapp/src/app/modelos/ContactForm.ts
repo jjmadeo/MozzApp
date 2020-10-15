29e1ee2d8ef69204eddd632a848b1c664d7955fd
jjmadeo <40671492+jjmadeo@users.noreply.github.com>
2020-10-14 21:44:24 -0300

    formulario

---
+++ front/Mozapp/src/app/modelos/ContactForm.ts
@@ -0,0 +1,10 @@
+ export class ContactForm {
+
+    constructor(
+        private nombre:string,
+        private apellido:string,
+        private email:String,
+        private telefono:string,
+        private mensaje:String
+    ){}
+}
--- front/Mozapp/src/app/moz/contacto/contacto.component.ts
+++ front/Mozapp/src/app/moz/contacto/contacto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { ContactForm } from "../../modelos/ContactForm";
+
+
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
+  contacto:ContactForm;
+
+   nombreData:string;
+   apellidoData:string;
+   emailData:String;
+   telefonoData:string;
+   mensajeData:String;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  EnviarFormContacto(){
+    this.contacto = new ContactForm(this.nombreData,this.apellidoData,this.emailData,this.telefonoData,this.mensajeData);
+    console.log(this.contacto)
+
+  }
+  onSubmit(f:NgForm){
+
   }
 
 }
