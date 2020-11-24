134f0aae5f00625871dead181e52afd35871349e
jjmadeo <40671492+jjmadeo@users.noreply.github.com>
2020-11-24 14:36:42 -0300

    asd

--- api/db.php
+++ api/db.php
@@ -7,7 +7,7 @@ $password = "";
 $db="mozapp";
 
 try {
-	$conn = new PDO("mysql:host=$servername;dbname=".$db, $username, $password);
+	$conn = new PDO("mysql:host=$servername;dbname=".$db.';charset=utf8', $username, $password);
     // set the PDO error mode to exception
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
