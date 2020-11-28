2770a56f40b4bfee308688882b91c02d92f5e126
salvador330 <salvadorcirino2004@hotmail.com>
2020-11-27 23:23:57 -0300

    actualizo validaciones html5

--- front/Mozapp/src/app/moz/Servicios/apiConfig.ts
+++ front/Mozapp/src/app/moz/Servicios/apiConfig.ts
@@ -1,11 +1,11 @@
 export  var config = 
 {
     "host":"http://localhost",
-    "port":81,
+    "port":80,
     "pathJuan":"/TPLab/api/",
     "pathSalva":"/TPlab/MozzApp/api/",
     "pathJose":"/Mozzapp/api/"
 }
-export var url = config.host+":"+config.port+config.pathJuan;
+// export var url = config.host+":"+config.port+config.pathJuan;
 // export var url = config.host+":"+config.port+config.pathJose;
-//  export var url = config.host+":"+config.port+config.pathSalva;
+ export var url = config.host+":"+config.port+config.pathSalva;
